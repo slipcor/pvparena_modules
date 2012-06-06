dd8810cbf5544ac6ae9ec4d3503f189d9f983404
Chris Fehling <chris@slipcor.de>
2012-06-06 02:41:52 +0200

    v0.8.6.14

--- m_RegionTool/src/net/slipcor/pvparena/modules/regiontool/RegionTool.java
+++ m_RegionTool/src/net/slipcor/pvparena/modules/regiontool/RegionTool.java
@@ -14,47 +14,48 @@ public class RegionTool extends ArenaModule {
 	public RegionTool() {
 		super("RegionTool");
 	}
-	
+
 	@Override
 	public String version() {
 		return "v0.8.6.14";
 	}
-	
+
 	public boolean onPlayerInteract(PlayerInteractEvent event) {
-		if (event.getPlayer().getItemInHand() == null || event.getPlayer().getItemInHand().getType() == Material.AIR) {
+		if (event.getPlayer().getItemInHand() == null
+				|| event.getPlayer().getItemInHand().getType() == Material.AIR) {
 			return false;
 		}
-		
+
 		if (event.getPlayer().getItemInHand().getType() == Material.AIR) {
 			return false;
 		}
-		
-		
+
 		for (Arena arena : Arenas.getArenas()) {
 			Material mMat = Material.STICK;
 			// TODO REPLACE WITH WAND!"
 			if (arena.cfg.get("setup.wand") != null) {
 				db.i("reading wand");
 				try {
-					mMat = Material
-							.getMaterial(arena.cfg.getInt("setup.wand"));
-					db.i("mMat now is " + mMat.name());
-					if (event.getPlayer().getItemInHand().getType() == mMat) {
-						Location loc = event.getPlayer().getLocation();
-						if (event.getClickedBlock() != null) {
-							loc = event.getClickedBlock().getLocation();
-						}
-						for (ArenaRegion region : arena.regions.values()) {
-							if (region.contains(loc)) {
-								Arenas.tellPlayer(event.getPlayer(), "§fArena §b" + 
-									arena.name + "§f: region §b" + region.name);
-							}
-						}
-					}
+					mMat = Material.getMaterial(arena.cfg.getInt("setup.wand"));
 				} catch (Exception e) {
 					db.i("exception reading ready block");
 					String sMat = arena.cfg.getString("setup.wand");
 					Language.log_warning("matnotfound", sMat);
+					return false;
+				}
+				db.i("mMat now is " + mMat.name());
+				if (event.getPlayer().getItemInHand().getType() == mMat) {
+					Location loc = event.getPlayer().getLocation();
+					if (event.getClickedBlock() != null) {
+						loc = event.getClickedBlock().getLocation();
+					}
+					for (ArenaRegion region : arena.regions.values()) {
+						if (region.contains(loc)) {
+							Arenas.tellPlayer(event.getPlayer(), "§fArena §b"
+									+ arena.name + "§f: region §b"
+									+ region.name);
+						}
+					}
 				}
 			}
 		}
