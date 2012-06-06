ef4a684b3ff611ac671a35288ca2dc6fa25bc64c
Chris Fehling <chris@slipcor.de>
2012-06-06 02:46:08 +0200

    v0.8.6.14 - finish the Skin module

--- m_Skins/src/net/slipcor/pvparena/modules/skins/Skins.java
+++ m_Skins/src/net/slipcor/pvparena/modules/skins/Skins.java
@@ -1,113 +1,80 @@
 package net.slipcor.pvparena.modules.skins;
 
-import java.util.HashMap;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.getspout.spoutapi.SpoutManager;
 
+import me.desmin88.mobdisguise.api.MobDisguiseAPI;
 import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.arena.ArenaPlayer;
 import net.slipcor.pvparena.arena.ArenaTeam;
 import net.slipcor.pvparena.core.Language;
-import net.slipcor.pvparena.managers.Arenas;
 import net.slipcor.pvparena.managers.Teams;
 import net.slipcor.pvparena.neworder.ArenaModule;
 
 public class Skins extends ArenaModule {
-	protected static String spoutHandler = null;
+	protected static boolean mdHandler = false;
 
 	public Skins() {
-		super("ColoredTeams");
+		super("Skins");
 	}
-	
+
 	@Override
 	public String version() {
-		return "v0.7.12.1";
+		return "v0.8.6.19";
 	}
 
-	@Override
-	public void addSettings(HashMap<String, String> types) {
-		types.put("game.hideName", "boolean");
-		types.put("game.mustbesafe", "boolean");
-		types.put("game.woolHead", "boolean");
-		types.put("game.woolFlagHead", "boolean");
-		types.put("messages.colorNick", "boolean");
-	}
-
-	private void colorizePlayer(Player player) {
-		db.i("colorizing player " + player.getName() + ";");
-
-		Arena arena = Arenas.getArenaByPlayer(player);
-		if (arena == null) {
-			db.i("> arena is null");
-			if (spoutHandler != null) {
-				SpoutManager.getPlayer(player).setTitle(player.getName());
-			}
-			return;
-		}
-
-		ArenaTeam team = Teams.getTeam(arena, ArenaPlayer.parsePlayer(player));
-		String n;
-		if (team == null) {
-			db.i("> team is null");
-			if (spoutHandler != null) {
-				SpoutManager.getPlayer(player).setTitle(player.getName());
-			}
-			return;
-		} else {
-			n = team.getColorString() + player.getName();
-		}
-		n = n.replaceAll("(&([a-f0-9]))", "§$2");
-		
-		player.setDisplayName(n);
-
-		if (arena.cfg.getBoolean("game.hideName")) {
-			n = " ";
-		}
-		if (spoutHandler != null) {
-			SpoutManager.getPlayer(player).setTitle(n);
-		}
-	}
-	
 	@Override
 	public void configParse(Arena arena, YamlConfiguration config, String type) {
-		config.addDefault("game.hideName", Boolean.valueOf(false));
-		config.addDefault("game.mustbesafe", Boolean.valueOf(true));
-		config.addDefault("game.woolHead", Boolean.valueOf(false));
-		config.addDefault("game.woolFlagHead", Boolean.valueOf(false));
-		config.addDefault("messages.colorNick", Boolean.valueOf(true));
-		config.options().copyDefaults(true);
+		if (config.get("skins") == null) {
+			for (ArenaTeam team : arena.getTeams()) {
+				String sName = team.getName();
+				config.addDefault("skins." + sName, "Herobrine");
+			}
+			config.options().copyDefaults(true);
+		}
 	}
-	
+
 	@Override
 	public void initLanguage(YamlConfiguration config) {
-		config.addDefault("log.nospout",
-				"Spout not found, you are missing some features ;)");
-		config.addDefault("log.spout", "Hooking into Spout!");
+		config.addDefault("log.nomd",
+				"MobDisguise not found, Skins module is inactive!");
+		config.addDefault("log.md", "Hooking into MobDisguise!");
 	}
-	
+
 	@Override
 	public void onEnable() {
-		if (Bukkit.getServer().getPluginManager().getPlugin("Spout") != null) {
-			spoutHandler = SpoutManager.getInstance().toString();
+		if (Bukkit.getServer().getPluginManager().getPlugin("MobDisguise") != null) {
+			mdHandler = true;
 		}
-		Language.log_info((spoutHandler == null) ? "nospout" : "spout");
+		Language.log_info((!mdHandler) ? "nomd" : "md");
 	}
 
 	@Override
 	public void tpPlayerToCoordName(Arena arena, Player player, String place) {
-		if (arena.cfg.getBoolean("messages.colorNick", true)) {
-			colorizePlayer(player);	
+		if (!mdHandler || !place.contains("lounge")) {
+			return;
 		}
-		
+		if (arena.isPartOf(player)) {
+			ArenaTeam team = Teams.getTeam(arena,
+					ArenaPlayer.parsePlayer(player));
+			if (team == null) {
+				return;
+			}
+			String disguise = arena.cfg.getString("skins." + team.getName());
+			
+			if (!MobDisguiseAPI.disguisePlayer(player, disguise)) {
+				if (!MobDisguiseAPI.disguisePlayerAsPlayer(player, disguise)) {
+					System.out.print("Unable to disguise " + player.getName() + " as " + disguise);
+				}
+			}
+		}
 	}
-	
+
 	@Override
 	public void unload(Player player) {
-		if (spoutHandler != null) {
-			SpoutManager.getPlayer(player).setTitle(player.getName());
+		if (mdHandler) {
+			MobDisguiseAPI.undisguisePlayer(player);
 		}
 	}
 }
