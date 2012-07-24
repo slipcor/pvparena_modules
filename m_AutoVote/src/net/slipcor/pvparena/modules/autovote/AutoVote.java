0c2bd69487ee7e1065a63f857678387ab3c95347
Chris Fehling <chris@slipcor.de>
2012-07-24 15:23:20 +0200

    v0.8.11.8 - unknown

--- m_AutoVote/src/net/slipcor/pvparena/modules/autovote/AutoVote.java
+++ m_AutoVote/src/net/slipcor/pvparena/modules/autovote/AutoVote.java
@@ -1,6 +1,7 @@
 package net.slipcor.pvparena.modules.autovote;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
@@ -10,7 +11,6 @@ import org.bukkit.entity.Player;
 import net.slipcor.pvparena.PVPArena;
 import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.command.PAAJoin;
-import net.slipcor.pvparena.command.PAA_Command;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.StringParser;
 import net.slipcor.pvparena.managers.Arenas;
@@ -20,19 +20,21 @@ import net.slipcor.pvparena.runnables.StartRunnable;
 public class AutoVote extends ArenaModule {
 	private static Arena a = null;
 	private static HashMap<String, String> votes = new HashMap<String, String>();
+
 	public AutoVote() {
-		super("AutoJoin");
+		super("AutoVote");
 	}
 
 	@Override
 	public String version() {
-		return "v0.8.11.0";
+		return "v0.8.11.8";
 	}
-	
+
 	@Override
 	public boolean checkJoin(Arena arena, Player player) {
 		if (a != null && !arena.equals(a)) {
-			Arenas.tellPlayer(player, Language.parse("arenarunning", arena.name));
+			Arenas.tellPlayer(player,
+					Language.parse("arenarunning", arena.name));
 			return false;
 		}
 		return true;
@@ -40,39 +42,50 @@ public class AutoVote extends ArenaModule {
 
 	@Override
 	public void addSettings(HashMap<String, String> types) {
-		types.put("autojoin.seconds", "int");
+		types.put("arenavote.seconds", "int");
+		types.put("arenavote.readyup", "int");
+		types.put("arenavote.everyone", "bool");
 	}
 
 	@Override
 	public void commitCommand(Arena arena, CommandSender sender, String[] args) {
 
-		if (!args[0].startsWith("vote") || !PAA_Command.checkArgs(sender, args, 2)) {
+		if (!args[0].startsWith("vote")) {
+			return;
+		}
+
+		votes.put(sender.getName(), arena.name);
+		Arenas.tellPlayer(sender, Language.parse("youvoted", arena.name));
+		
+		if (!arena.cfg.getBoolean("arenavote.everyone")) {
 			return;
 		}
 		
-		Arena aA = Arenas.getArenaByName(args[1]);
-		
-		if (aA == null) {
-			Arenas.tellPlayer(sender, Language.parse("unknownarena") + ": " + args[1]);
-			return;
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			if (p == null) {
+				continue;
+			}
+			Arenas.tellPlayer(p, Language.parse("playervoted", arena.name, sender.getName()));
 		}
-		
-		votes.put(sender.getName(), aA.name);
-		Arenas.tellPlayer(sender, Language.parse("youvoted",aA.name));
-		
-		return;
 	}
 
 	@Override
 	public void configParse(Arena arena, YamlConfiguration config, String type) {
-		config.addDefault("autojoin.seconds", Integer.valueOf(30));
+		config.addDefault("arenavote.seconds", Integer.valueOf(30));
+		config.addDefault("arenavote.readyup", Integer.valueOf(30));
+		config.addDefault("arenavote.everyone", Boolean.valueOf(false));
 		config.options().copyDefaults(true);
 	}
 
 	public void initLanguage(YamlConfiguration config) {
 		config.addDefault("lang.arenarunning", "Arena running: %1%");
 		config.addDefault("lang.autojoin", "Arena auto join started!");
+		config.addDefault(
+				"lang.votenow",
+				"Vote for your arena! %1% left!\nVote with /pa [arenaname] vote\nAvailable arenas: "
+						+ Arenas.getNames());
 		config.addDefault("lang.youvoted", "You voted for arena %1%!");
+		config.addDefault("lang.playervoted", "%2% voted for arena %1%!");
 	}
 
 	@Override
@@ -83,12 +96,10 @@ public class AutoVote extends ArenaModule {
 	@Override
 	public void parseInfo(Arena arena, CommandSender player) {
 		player.sendMessage("");
-		player.sendMessage("§6AutoJoin:§f "
-				+ StringParser.colorVar(arena.cfg
-						.getBoolean("autojoin.autojoin"))
-				+ "("
-				+ StringParser.colorVar(arena.cfg
-						.getInt("autojoin.seconds")) + ")");
+		player.sendMessage("§6ArenaVote:§f "
+				+ StringParser.colorVar(arena.cfg.getInt("arenavote.seconds"))
+				+ " | "
+				+ StringParser.colorVar(arena.cfg.getInt("arenavote.readyup")));
 	}
 
 	@Override
@@ -96,8 +107,10 @@ public class AutoVote extends ArenaModule {
 		votes.clear();
 		a = null;
 
-		AutoVoteRunnable fr = new AutoVoteRunnable(a, 5, 5);
-		fr.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(PVPArena.instance, fr, 20L, 20L));
+		AutoVoteRunnable fr = new AutoVoteRunnable(
+				arena.cfg.getInt("arenavote.seconds"), 0);
+		fr.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(
+				PVPArena.instance, fr, 20L, 20L));
 	}
 
 	public static void commit() {
@@ -108,40 +121,54 @@ public class AutoVote extends ArenaModule {
 			if (counts.containsKey(name)) {
 				i = counts.get(name);
 			}
-			
+
 			counts.put(name, ++i);
-			
+
 			if (i > max) {
 				max = i;
 			}
 		}
-		
+
 		for (String name : counts.keySet()) {
 			if (counts.get(name) == max) {
 				a = Arenas.getArenaByName(name);
 			}
 		}
-		
+
 		if (a == null) {
 			a = Arenas.getFirst();
 		}
-		
+
 		if (a == null) {
 			return;
 		}
 
 		PAAJoin pj = new PAAJoin();
+
+		HashSet<String> toTeleport = new HashSet<String>();
 		
-		for (String pName : votes.keySet()) {
+		if (a.cfg.getBoolean("arenavote.everyone")) {
+			for (Player p : Bukkit.getOnlinePlayers()) {
+				toTeleport.add(p.getName());
+			}
+		} else {
+			for (String s : votes.keySet()) {
+				toTeleport.add(s);
+			}
+		}
+		
+		for (String pName : toTeleport) {
 			Player p = Bukkit.getPlayerExact(pName);
 			if (p == null) {
 				continue;
 			}
-			
+
 			pj.commit(a, p, null);
 		}
-		
-		StartRunnable fr = new StartRunnable(a, 5, 5);
-		fr.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(PVPArena.instance, fr, 20L, 20L));
+
+		StartRunnable fr = new StartRunnable(a,
+				a.cfg.getInt("arenavote.readyup"), 0);
+		fr.setId(Bukkit.getScheduler().scheduleSyncRepeatingTask(
+				PVPArena.instance, fr, 20L, 20L));
 	}
 }
--- m_AutoVote/src/net/slipcor/pvparena/modules/autovote/AutoVoteRunnable.java
+++ m_AutoVote/src/net/slipcor/pvparena/modules/autovote/AutoVoteRunnable.java
@@ -2,28 +2,24 @@ package net.slipcor.pvparena.modules.autovote;
 
 import org.bukkit.Bukkit;
 
-import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.core.Debug;
 import net.slipcor.pvparena.runnables.TimerInfo;
 
 public class AutoVoteRunnable implements Runnable {
-	private final Arena a;
 	private Debug db = new Debug(68);
 	private int id;
 
 	private int count = 0;
-	public AutoVoteRunnable(Arena a, int i, int iid) {
-		this.a = a;
+	public AutoVoteRunnable(int i, int iid) {
 		id = 0;
 		count = i+1;
-		db.i("AutoJoinRunnable constructor");
+		db.i("ArenaVoteRunnable constructor");
 	}
 
 	@Override
 	public void run() {
-		TimerInfo.spam("votenow", --count, null, a, false);
+		TimerInfo.spam("votenow", --count, null, null, true);
 		if (count == 0) {
-			Bukkit.getScheduler().cancelTask(a.START_ID);
 			commit();
 		} if (count < 0) {
 			System.out.print("running");
@@ -31,7 +27,7 @@ public class AutoVoteRunnable implements Runnable {
 	}
 
 	private void commit() {
-		db.i("AutoJoinRunnable commiting");
+		db.i("ArenaVoteRunnable commiting");
 		Bukkit.getScheduler().cancelTask(id);
 		AutoVote.commit();
 	}
