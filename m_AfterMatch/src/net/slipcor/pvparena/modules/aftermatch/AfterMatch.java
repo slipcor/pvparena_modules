cb22b13a4fd1c2f4b1198d80343a895661465937
Chris Fehling <chris@slipcor.de>
2012-12-06 01:23:13 +0100

    v0.10.0.0 - module management rewrite

--- m_AfterMatch/src/net/slipcor/pvparena/modules/aftermatch/AfterMatch.java
+++ m_AfterMatch/src/net/slipcor/pvparena/modules/aftermatch/AfterMatch.java
@@ -1,7 +1,5 @@
 package net.slipcor.pvparena.modules.aftermatch;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -11,68 +9,148 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import net.slipcor.pvparena.PVPArena;
-import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.arena.ArenaPlayer;
 import net.slipcor.pvparena.arena.ArenaPlayer.Status;
 import net.slipcor.pvparena.arena.ArenaTeam;
+import net.slipcor.pvparena.commands.PAA_Install;
 import net.slipcor.pvparena.core.Config.CFG;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.Language.MSG;
 import net.slipcor.pvparena.core.StringParser;
 import net.slipcor.pvparena.loadables.ArenaModule;
 
-public class AfterMatch extends ArenaModule {
-	protected HashMap<Arena, Integer> runnables = new HashMap<Arena, Integer>();
+public class AfterMatch extends ArenaModule implements Cloneable {
+	protected Integer runId = null;
+	private boolean aftermatch = false;
 
 	public AfterMatch() {
 		super("AfterMatch");
 	}
 	
-	private HashSet<Arena> aftermatchs = new HashSet<Arena>();
-	
 
 	@Override
 	public String version() {
-		return "v0.9.6.16";
+		return "v0.10.0.0";
 	}
 
-	public void afterMatch(Arena a) {
-		for (ArenaTeam t : a.getTeams()) {
+	public void afterMatch() {
+		for (ArenaTeam t : arena.getTeams()) {
 			for (ArenaPlayer p : t.getTeamMembers()) {
 				if (!p.getStatus().equals(Status.FIGHT)) {
 					continue;
 				}
 				Player player = p.get();
-				a.tpPlayerToCoordName(player, "after");
+				arena.tpPlayerToCoordName(player, "after");
 			}
 		}
-		a.broadcast(Language.parse(MSG.MODULE_AFTERMATCH_STARTING));
-		PVPArena.instance.getAgm().setPlayerLives(a, 0);
-		aftermatchs.add(a);
+		arena.broadcast(Language.parse(MSG.MODULE_AFTERMATCH_STARTING));
+		PVPArena.instance.getAgm().setPlayerLives(arena, 0);
+		aftermatch = true;
 	}
-
+	
 	@Override
-	public String checkForMissingSpawns(Arena arena, Set<String> list) {
-		
+	public boolean checkCommand(String s) {
+		return s.equals("!am") || s.equals("aftermatch");
+	}
+	
+	@Override
+	public String checkForMissingSpawns(Set<String> list) {
 		for (String s : list) {
 			if (s.startsWith("after")) {
 				return null;
 			}
 		}
-		return MSG.MODULE_AFTERMATCH_SPAWNNOTSET.toString();
+		return Language.parse(MSG.MODULE_AFTERMATCH_SPAWNNOTSET);
+	}
+	
+	@Override
+	public void commitCommand(CommandSender sender, String[] args) {
+		// !am time 6
+		// !am death 4
+		
+		if (!PVPArena.hasAdminPerms(sender)
+				&& !(PVPArena.hasCreatePerms(sender, arena))) {
+			arena.msg(
+					sender,
+					Language.parse(MSG.ERROR_NOPERM,
+							Language.parse(MSG.ERROR_NOPERM_X_ADMIN)));
+			return;
+		}
+
+		if (!(new PAA_Install()).argCountValid(sender, args, new Integer[] { 2,3 })) {
+			return;
+		}
+		
+		if (args[0].equals("!am") || args[0].equals("aftermatch")) {
+			if (args.length == 2) {
+				if (args[1].equals("off")) {
+					arena.getArenaConfig().set(CFG.MODULES_AFTERMATCH_AFTERMATCH, args[1]);
+					arena.getArenaConfig().save();
+					arena.msg(sender, Language.parse(MSG.SET_DONE, CFG.MODULES_AFTERMATCH_AFTERMATCH.getNode(), args[1]));
+					return;
+				}
+				arena.msg(sender, Language.parse(MSG.ERROR_ARGUMENT, args[1], "off"));
+				return;
+			}
+			int i = 0;
+			try {
+				i = Integer.parseInt(args[2]);
+			} catch (Exception e) {
+				arena.msg(sender,
+						Language.parse(MSG.ERROR_NOT_NUMERIC, args[2]));
+				return;
+			}
+			if (args[1].equals("time") || args[1].equals("death")) {
+				arena.getArenaConfig().set(CFG.MODULES_AFTERMATCH_AFTERMATCH, args[1]+":"+i);
+				arena.getArenaConfig().save();
+				arena.msg(sender, Language.parse(MSG.SET_DONE, CFG.MODULES_AFTERMATCH_AFTERMATCH.getNode(), args[1]+":"+i));
+				return;
+			}
+			
+			arena.msg(sender, Language.parse(MSG.ERROR_ARGUMENT, args[1], "time | death"));
+			return;
+		}
 	}
 
 	@Override
-	public void parsePlayerDeath(Arena arena, Player player,
+	public void configParse(YamlConfiguration config) {
+		String pu = config.getString(CFG.MODULES_AFTERMATCH_AFTERMATCH.getNode(), "off");
+
+		//String[] ss = pu.split(":");
+		if (pu.startsWith("death")) {
+			//
+		} else if (pu.startsWith("time")) {
+			// later
+		} else {
+			db.w("error activating aftermatch module");
+		}
+	}
+
+	@Override
+	public void displayInfo(CommandSender player) {
+		player.sendMessage("§bAfterMatch:§f "
+				+ StringParser.colorVar(!arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH).equals("off"))
+				+ "("
+				+ StringParser.colorVar(arena.getArenaConfig()
+						.getString(CFG.MODULES_AFTERMATCH_AFTERMATCH)) + ")");
+	}
+	
+	@Override
+	public boolean hasSpawn(String string) {
+		return string.equals("after");
+	}
+
+	@Override
+	public void parsePlayerDeath(Player player,
 			EntityDamageEvent cause) {
 		String pu = arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH);
 
-		if (pu.equals("off") || aftermatchs.contains(arena)) {
+		if (pu.equals("off") || aftermatch) {
 			return;
 		}
 		
 		String[] ss = pu.split(":");
-		if (pu.startsWith("time") || runnables.containsKey(arena)) {
+		if (pu.startsWith("time") || runId != null) {
 			return;
 		}
 
@@ -89,68 +167,32 @@ public class AfterMatch extends ArenaModule {
 			}
 		}
 
-		runnables.put(arena, 0);
+		runId = null;
 
-		afterMatch(arena);
+		afterMatch();
 	}
 
 	@Override
-	public void configParse(Arena arena, YamlConfiguration config) {
-		String pu = config.getString("aftermatch.aftermatch", "off");
-
-		//String[] ss = pu.split(":");
-		if (pu.startsWith("death")) {
-			//
-		} else if (pu.startsWith("time")) {
-			// later
-		} else {
-			db.w("error activating aftermatch module");
-		}
-
-		config.addDefault("aftermatch.aftermatch", "off");
-		config.options().copyDefaults(true);
-	}
-
-	@Override
-	public void displayInfo(Arena arena, CommandSender player) {
-		player.sendMessage("§bAfterMatch:§f "
-				+ StringParser.colorVar(!arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH).equals("off"))
-				+ "("
-				+ StringParser.colorVar(arena.getArenaConfig()
-						.getString(CFG.MODULES_AFTERMATCH_AFTERMATCH)) + ")");
-	}
-	
-	@Override
-	public boolean hasSpawn(Arena arena, String string) {
-		return string.equals("after");
-	}
-	
-	@Override
-	public boolean isActive(Arena arena) {
-		return (arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH).contains(":"));
-	}
-
-	@Override
-	public void reset(Arena arena, boolean force) {
+	public void reset(boolean force) {
 		String pu = arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH);
 
 		if (pu.equals("off")) {
 			return;
 		}
-		if (runnables.containsKey(arena)) {
-			Bukkit.getScheduler().cancelTask(runnables.get(arena));
-			runnables.remove(arena);
+		if (runId != null) {
+			Bukkit.getScheduler().cancelTask(runId);
+			runId = null;
 		}
-		aftermatchs.remove(arena);
+		aftermatch = false;
 	}
 
 	@Override
-	public void teleportAllToSpawn(Arena arena) {
+	public void parseStart() {
 		String pu = arena.getArenaConfig().getString(CFG.MODULES_AFTERMATCH_AFTERMATCH);
 
-		if (runnables.containsKey(arena)) {
-			Bukkit.getScheduler().cancelTask(runnables.get(arena));
-			runnables.remove(arena);
+		if (runId != null) {
+			Bukkit.getScheduler().cancelTask(runId);
+			runId = null;
 		}
 
 		int i = 0;
@@ -167,11 +209,7 @@ public class AfterMatch extends ArenaModule {
 				+ i);
 		if (i > 0) {
 			db.i("aftermatch time trigger!");
-			runnables.put(
-					arena,
-					Bukkit.getScheduler().scheduleSyncDelayedTask(
-							PVPArena.instance, new AfterRunnable(arena, this),
-							i * 20L));
+			new AfterRunnable(this, arena.getArenaConfig().getInt(CFG.MODULES_AFTERMATCH_AFTERMATCH));
 		}
 	}
 }
