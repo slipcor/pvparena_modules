3c23fcb5aa0278c341109f1f00ff9b6f6c5c252a
Chris Fehling <chris@slipcor.de>
2012-12-08 00:37:47 +0100

    v0.10.0.0 - module system rewrite

--- m_FixInventoryLoss/src/net/slipcor/pvparena/modules/fixes/InventoryLoss.java
+++ m_FixInventoryLoss/src/net/slipcor/pvparena/modules/fixes/InventoryLoss.java
@@ -6,13 +6,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import net.slipcor.pvparena.arena.Arena;
+import net.slipcor.pvparena.PVPArena;
 import net.slipcor.pvparena.classes.PACheck;
+import net.slipcor.pvparena.commands.PAA__Command;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.Config.CFG;
 import net.slipcor.pvparena.core.Language.MSG;
 import net.slipcor.pvparena.core.StringParser;
-import net.slipcor.pvparena.managers.ArenaManager;
 import net.slipcor.pvparena.loadables.ArenaModule;
 
 public class InventoryLoss extends ArenaModule {
@@ -23,11 +23,16 @@ public class InventoryLoss extends ArenaModule {
 
 	@Override
 	public String version() {
-		return "v0.9.6.16";
+		return "v0.10.0.0";
+	}
+	
+	@Override
+	public boolean checkCommand(String s) {
+		return s.equals("!fil") || s.equals("fixinventoryloss");
 	}
 
 	@Override
-	public PACheck checkJoin(Arena arena, CommandSender sender,
+	public PACheck checkJoin(CommandSender sender,
 			PACheck res, boolean b) {
 		Player player = (Player) sender;
 		int priority = 5;
@@ -38,7 +43,7 @@ public class InventoryLoss extends ArenaModule {
 		
 		if (arena.getArenaConfig().getBoolean(CFG.MODULES_FIXINVENTORYLOSS_GAMEMODE)) {
 			if (!player.getGameMode().equals(GameMode.SURVIVAL)) {
-				ArenaManager.tellPlayer(player, Language.parse(MSG.MODULE_FIXINVENTORYLOSS_GAMEMODE));
+				res.setError(this, Language.parse(MSG.MODULE_FIXINVENTORYLOSS_GAMEMODE));
 				return res;
 			}
 		}
@@ -58,18 +63,50 @@ public class InventoryLoss extends ArenaModule {
 		}
 		return res;
 	}
-
-	@Override
-	public void displayInfo(Arena arena, CommandSender player) {
-		player.sendMessage("");
-		player.sendMessage("§6FixInventoryLoss:§f "
-				+ StringParser.colorVar("emptyInventory", arena.getArenaConfig().getBoolean(CFG.MODULES_FIXINVENTORYLOSS_INVENTORY))
-				+ " || "
-				+ StringParser.colorVar("gamemodeSurvival", arena.getArenaConfig().getBoolean(CFG.MODULES_FIXINVENTORYLOSS_GAMEMODE)));
-	}
 	
 	@Override
-	public boolean isActive(Arena a) {
-		return true;
+	public void commitCommand(CommandSender sender, String[] args) {
+		// !fil [value]
+		
+		if (!PVPArena.hasAdminPerms(sender)
+				&& !(PVPArena.hasCreatePerms(sender, arena))) {
+			arena.msg(
+					sender,
+					Language.parse(MSG.ERROR_NOPERM,
+							Language.parse(MSG.ERROR_NOPERM_X_ADMIN)));
+			return;
+		}
+
+		if (!PAA__Command.argCountValid(sender, arena, args, new Integer[] { 2 })) {
+			return;
+		}
+		
+		CFG c = null;
+		
+		if (args[1].equals("gamemode")) {
+			c = CFG.MODULES_FIXINVENTORYLOSS_GAMEMODE;
+		} else if (args[1].equals("inventory")) {
+			c = CFG.MODULES_FIXINVENTORYLOSS_INVENTORY;
+		}
+		
+		if (c == null) {
+			arena.msg(sender, Language.parse(MSG.ERROR_ARGUMENT, args[1], "gamemode | inventory"));
+			return;
+		}
+		
+		boolean b = arena.getArenaConfig().getBoolean(c);
+		arena.getArenaConfig().set(c, !b);
+		arena.getArenaConfig().save();
+		arena.msg(sender, Language.parse(MSG.SET_DONE, c.getNode(), String.valueOf(!b)));
+		
+	}
+
+	@Override
+	public void displayInfo(CommandSender player) {
+		player.sendMessage("");
+		player.sendMessage("§6FixInventoryLoss:§f "
+				+ StringParser.colorVar("inventory", arena.getArenaConfig().getBoolean(CFG.MODULES_FIXINVENTORYLOSS_INVENTORY))
+				+ " || "
+				+ StringParser.colorVar("gamemode", arena.getArenaConfig().getBoolean(CFG.MODULES_FIXINVENTORYLOSS_GAMEMODE)));
 	}
 }
