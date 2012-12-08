da372d6957c18f96ba0078caeb2ed15ee44f07eb
Chris Fehling <chris@slipcor.de>
2012-12-08 14:22:56 +0100

    v0.10.0.0 - module system rewrite

--- m_StartFreeze/src/net/slipcor/pvparena/modules/startfreeze/StartFreeze.java
+++ m_StartFreeze/src/net/slipcor/pvparena/modules/startfreeze/StartFreeze.java
@@ -1,24 +1,24 @@
 package net.slipcor.pvparena.modules.startfreeze;
 
-import java.util.HashMap;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import net.slipcor.pvparena.PVPArena;
-import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.arena.ArenaPlayer;
+import net.slipcor.pvparena.commands.PAA__Command;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.Config.CFG;
 import net.slipcor.pvparena.core.Language.MSG;
 import net.slipcor.pvparena.loadables.ArenaModule;
 
 public class StartFreeze extends ArenaModule implements Listener {
-	protected static HashMap<Arena, StartFreezer> runnables = new HashMap<Arena, StartFreezer>();
-	protected static HashMap<Arena, Integer> ids = new HashMap<Arena, Integer>();
+	protected static StartFreezer runnable = null;
+	protected static int id = -1;
 
 	public StartFreeze() {
 		super("StartFreeze");
@@ -26,36 +26,68 @@ public class StartFreeze extends ArenaModule implements Listener {
 
 	@Override
 	public String version() {
-		return "v0.9.6.16";
+		return "v0.10.0.0";
 	}
 	
 	@Override
-	public boolean isActive(Arena arena) {
-		return arena.getArenaConfig().getInt(CFG.MODULES_STARTFREEZE_TIMER) > -1;
+	public boolean checkCommand(String s) {
+		return s.equals("startfreeze") || s.equals("!sf");
+	}
+	
+	@Override
+	public void commitCommand(CommandSender sender, String[] args) {
+		// !sf 5
+		
+		if (!PVPArena.hasAdminPerms(sender)
+				&& !(PVPArena.hasCreatePerms(sender, arena))) {
+			arena.msg(
+					sender,
+					Language.parse(MSG.ERROR_NOPERM,
+							Language.parse(MSG.ERROR_NOPERM_X_ADMIN)));
+			return;
+		}
+
+		if (!PAA__Command.argCountValid(sender, arena, args, new Integer[] { 2 })) {
+			return;
+		}
+		
+		if (args[0].equals("!sf") || args[0].equals("startfreeze")) {
+			int i = 0;
+			try {
+				i = Integer.parseInt(args[1]);
+			} catch (Exception e) {
+				arena.msg(sender,
+						Language.parse(MSG.ERROR_NOT_NUMERIC, args[1]));
+				return;
+			}
+			
+			arena.getArenaConfig().set(CFG.MODULES_STARTFREEZE_TIMER, i);
+			arena.getArenaConfig().save();
+			arena.msg(sender, Language.parse(MSG.SET_DONE, CFG.MODULES_STARTFREEZE_TIMER.getNode(), String.valueOf(i)));
+		}
 	}
 
 	@Override
-	public void reset(Arena arena, boolean force) {
-		if (runnables.containsKey(arena))
-			Bukkit.getScheduler().cancelTask(ids.get(arena));
-		runnables.remove(arena);
-		ids.remove(arena);
+	public void reset(boolean force) {
+		if (runnable != null)
+			Bukkit.getScheduler().cancelTask(id);
+		runnable = null;
+		id = -1;
 	}
 
 	@Override
-	public void teleportAllToSpawn(Arena arena) {
-		StartFreezer sf = new StartFreezer(arena);
-		runnables.put(arena, sf);
-		ids.put(arena,
+	public void parseStart() {
+		runnable = new StartFreezer();
+		id = 
 				Bukkit.getScheduler().scheduleSyncDelayedTask(
-						PVPArena.instance, sf,
-						arena.getArenaConfig().getInt(CFG.MODULES_STARTFREEZE_TIMER) * 20L));
+						PVPArena.instance, runnable,
+						arena.getArenaConfig().getInt(CFG.MODULES_STARTFREEZE_TIMER) * 20L);
 		arena.broadcast(Language.parse(MSG.MODULE_STARTFREEZE_ANNOUNCE,
 				String.valueOf(arena.getArenaConfig().getInt(CFG.MODULES_STARTFREEZE_TIMER))));
 	}
 
 	@Override
-	public void onEnable() {
+	public void parseEnable() {
 		Bukkit.getPluginManager().registerEvents(this, PVPArena.instance);
 	}
 
@@ -63,10 +95,10 @@ public class StartFreeze extends ArenaModule implements Listener {
 	public void onPlayerMove(PlayerMoveEvent event) {
 		Player p = event.getPlayer();
 		ArenaPlayer ap = ArenaPlayer.parsePlayer(p.getName());
-		if (ap.getArena() == null || !isActive(ap.getArena())) {
+		if (ap.getArena() == null || (!arena.equals(ap.getArena()))) {
 			return;
 		}
-		if (runnables.containsKey(ap.getArena())) {
+		if (runnable != null) {
 			Location from = event.getFrom();
 			Location to = event.getTo();
 			if ((from.getBlockX() != to.getBlockX()) ||
--- m_StartFreeze/src/net/slipcor/pvparena/modules/startfreeze/StartFreezer.java
+++ m_StartFreeze/src/net/slipcor/pvparena/modules/startfreeze/StartFreezer.java
@@ -1,20 +1,13 @@
 package net.slipcor.pvparena.modules.startfreeze;
 
-import net.slipcor.pvparena.arena.Arena;
-
 public class StartFreezer implements Runnable {
-	private final Arena a;
-
-	public StartFreezer(Arena a) {
-		this.a = a;
-	}
 
 	/**
 	 * the run method, commit arena end
 	 */
 	@Override
 	public void run() {
-		StartFreeze.ids.remove(a);
-		StartFreeze.runnables.remove(a);
+		StartFreeze.id = -1;
+		StartFreeze.runnable = null;
 	}
 }
