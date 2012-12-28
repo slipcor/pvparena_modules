3352c1fc34600e1e6225c7b7087770aa507dc152
Chris Fehling <chris@slipcor.de>
2012-12-28 20:55:31 +0100

    v0.10.2.9 - RespawnRelay implementation

---
+++ m_RespawnRelay/src/net/slipcor/pvparena/modules/respawnrelay/RelayRunnable.java
@@ -0,0 +1,46 @@
+package net.slipcor.pvparena.modules.respawnrelay;
+
+import java.util.List;
+
+import org.bukkit.inventory.ItemStack;
+
+import net.slipcor.pvparena.PVPArena;
+import net.slipcor.pvparena.arena.Arena;
+import net.slipcor.pvparena.arena.ArenaPlayer;
+import net.slipcor.pvparena.arena.ArenaPlayer.Status;
+import net.slipcor.pvparena.core.Debug;
+import net.slipcor.pvparena.core.Language.MSG;
+import net.slipcor.pvparena.managers.SpawnManager;
+import net.slipcor.pvparena.runnables.ArenaRunnable;
+import net.slipcor.pvparena.runnables.InventoryRefillRunnable;
+
+public class RelayRunnable extends ArenaRunnable {
+	private Arena a;
+	private ArenaPlayer ap;
+	List<ItemStack> drops;
+	private Debug db = new Debug(77);
+
+	public RelayRunnable(Arena arena, ArenaPlayer ap, List<ItemStack> drops) {
+		
+		super(MSG.TIMER_STARTING_IN.getNode(), 10, ap.get(), null, false);
+		
+		a = arena;
+		this.ap = ap;
+		this.drops = drops;
+	}
+
+	@Override
+	protected void commit() {
+		db.i("RelayRunnable commiting", ap.getName());
+		new InventoryRefillRunnable(a, ap.get(), drops);
+		SpawnManager.respawn(a,  ap);
+		a.unKillPlayer(ap.get(), ap.get().getLastDamageCause()==null?null:ap.get().getLastDamageCause().getCause(), ap.get().getKiller());
+		ap.setStatus(Status.FIGHT);
+		
+	}
+
+	@Override
+	protected void warn() {
+		PVPArena.instance.getLogger().warning("RelayRunnable not scheduled yet!");
+	}
+}
---
+++ m_RespawnRelay/src/net/slipcor/pvparena/modules/respawnrelay/RespawnRelay.java
@@ -0,0 +1,64 @@
+package net.slipcor.pvparena.modules.respawnrelay;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import net.slipcor.pvparena.arena.ArenaPlayer;
+import net.slipcor.pvparena.arena.ArenaPlayer.Status;
+import net.slipcor.pvparena.loadables.ArenaModule;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class RespawnRelay extends ArenaModule {
+	HashMap<String, BukkitRunnable> runners = new HashMap<String, BukkitRunnable>();
+	
+	public RespawnRelay() {
+		super("RespawnRelay");
+	}
+	
+	@Override
+	public String version() {
+		return "v0.10.2.7";
+	}
+	
+	@Override
+	public boolean hasSpawn(String s) {
+		return s.equals("relay");
+	}
+	
+	@Override
+	public String checkForMissingSpawns(Set<String> list) {
+		return list.contains("relay")?null:"relay not set";
+	}
+	
+	@Override
+	public boolean tryDeathOverride(ArenaPlayer ap, List<ItemStack> drops) {
+		ap.setStatus(Status.DEAD);
+		
+		if (drops == null) {
+			drops = new ArrayList<ItemStack>();
+		}
+		
+		arena.tpPlayerToCoordName(ap.get(), "relay");
+		arena.unKillPlayer(ap.get(), ap.get().getLastDamageCause()==null?null:ap.get().getLastDamageCause().getCause(), ap.get().getKiller());
+		
+		if (runners.containsKey(ap.getName())) {
+			return true;
+		}
+		
+		runners.put(ap.getName(), new RelayRunnable(arena, ap, drops));
+		
+		return true;
+	}
+	
+	@Override
+	public void reset(boolean force) {
+		for (BukkitRunnable br : runners.values()) {
+			br.cancel();
+		}
+		runners.clear();
+	}
+}
