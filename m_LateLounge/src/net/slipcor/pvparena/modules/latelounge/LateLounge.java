5149de7ecbc4273fb225203adcc3458573b01b35
slipcor <chris@slipcor.de>
2013-02-23 15:54:52 +0100

    v1.0.1.58 - update LateLounge to display queue position

--- m_LateLounge/src/net/slipcor/pvparena/modules/latelounge/LateLounge.java
+++ m_LateLounge/src/net/slipcor/pvparena/modules/latelounge/LateLounge.java
@@ -1,13 +1,12 @@
 package net.slipcor.pvparena.modules.latelounge;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
-
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.classes.PACheck;
 import net.slipcor.pvparena.commands.AbstractArenaCommand;
 import net.slipcor.pvparena.commands.PAG_Join;
@@ -25,10 +24,10 @@ public class LateLounge extends ArenaModule {
 	
 	@Override
 	public String version() {
-		return "v1.0.0.25";
+		return "v1.0.1.58";
 	}
 	
-	private Set<String> playerSet = null;
+	private List<String> playerList = null;
 	
 	/**
 	 * hook into a player trying to join the arena
@@ -48,16 +47,27 @@ public class LateLounge extends ArenaModule {
 		
 		Player player = (Player) sender;
 		
-		if (getPlayerSet().contains(player.getName())) {
-			if (getPlayerSet().size() < arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS)) {
+		if (getPlayerList().contains(player.getName())) {
+			if (getPlayerList().size() < arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS)) {
 				res.setError(this, Language.parse(MSG.MODULE_LATELOUNGE_WAIT));
+				int pos = 1;
+				
+				for (String name : getPlayerList()) {
+					if (name.equals(player.getName())) {
+						break;
+					}
+					pos++;
+				}
+				
+				arena.msg(player, Language.parse(MSG.MODULE_LATELOUNGE_POSITION, String.valueOf(pos)));
 				return res;
 			}
 		}
 		
-		if (arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS) > getPlayerSet().size() + 1) {
+		if (arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS) > getPlayerList().size() + 1) {
 			// not enough players
-			getPlayerSet().add(player.getName());
+			getPlayerList().add(player.getName());
+			final int pos = getPlayerList().size();
 			Player[] aPlayers = Bukkit.getOnlinePlayers();
 			
 			for (Player p : aPlayers) {
@@ -65,20 +75,22 @@ public class LateLounge extends ArenaModule {
 					continue;
 				}
 				try {
-					Arena.pmsg(p, Language.parse(MSG.MODULE_LATELOUNGE_ANNOUNCE, arena.getName(), player.getName()));
+					arena.msg(p, Language.parse(MSG.MODULE_LATELOUNGE_ANNOUNCE, arena.getName(), player.getName()));
+					
 				} catch (Exception e) {
 					//
 				}
 			}
+			arena.msg(player, Language.parse(MSG.MODULE_LATELOUNGE_POSITION, String.valueOf(pos)));
 			res.setError(this, Language.parse(MSG.MODULE_LATELOUNGE_WAIT));
 			return res;
-		} else if (arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS) == getPlayerSet().size() + 1) {
+		} else if (arena.getArenaConfig().getInt(CFG.READY_MINPLAYERS) == getPlayerList().size() + 1) {
 			// not enough players
-			getPlayerSet().add(player.getName());
+			getPlayerList().add(player.getName());
 			
 			HashSet<String> removals = new HashSet<String>();
 			
-			for (String s : getPlayerSet()) {
+			for (String s : getPlayerList()) {
 				Player p = Bukkit.getPlayerExact(s);
 				
 				boolean removeMe = false;
@@ -104,11 +116,11 @@ public class LateLounge extends ArenaModule {
 
 			if (removals.size() > 0) {
 				for (String s : removals) {
-					getPlayerSet().remove(s);
+					getPlayerList().remove(s);
 				}
 			} else {
 				// SUCCESS!
-				for (String s : getPlayerSet()) {
+				for (String s : getPlayerList()) {
 					if (s.equals(sender.getName())) {
 						continue;
 					}
@@ -124,15 +136,15 @@ public class LateLounge extends ArenaModule {
 		return res;
 	}
 	
-	private Set<String> getPlayerSet() {
-		if (playerSet == null) {
-			playerSet = new HashSet<String>();
+	private List<String> getPlayerList() {
+		if (playerList == null) {
+			playerList = new ArrayList<String>();
 		}
-		return playerSet;
+		return playerList;
 	}
 	
 	@Override
 	public void reset(boolean force) {
-		getPlayerSet().clear();
+		getPlayerList().clear();
 	}
 }
