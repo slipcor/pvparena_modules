255156d2db05ebaeeff95fdf2c1c9f7899684161
SLiPCoR <chris@slipcor.de>
2014-03-07 22:25:29 +0100

    v1.1.0.395 - add one more debugging error display

--- m_Vault/src/net/slipcor/pvparena/modules/vaultsupport/VaultSupport.java
+++ m_Vault/src/net/slipcor/pvparena/modules/vaultsupport/VaultSupport.java
@@ -46,7 +46,7 @@ public class VaultSupport extends ArenaModule implements Listener {
 
 	@Override
 	public String version() {
-		return "v1.1.0.388";
+		return "v1.1.0.395";
 	}
 
 	@Override
@@ -752,7 +752,7 @@ public class VaultSupport extends ArenaModule implements Listener {
 			
 			economy.depositPlayer(playerName, value);
 		} catch (Exception e) {
-			
+			e.printStackTrace();
 		}
 	}
 }
