c95860cb25713a691a161bcceae3d22bf5774d62
slipcor <chris@slipcor.de>
2016-01-01 00:07:00 +0100

    v1.3.1.39 - prevent players (apart from the dueling people) from joining

--- m_Duel/src/net/slipcor/pvparena/modules/duel/DuelManager.java
+++ m_Duel/src/net/slipcor/pvparena/modules/duel/DuelManager.java
@@ -3,6 +3,7 @@ package net.slipcor.pvparena.modules.duel;
 import net.slipcor.pvparena.PVPArena;
 import net.slipcor.pvparena.arena.Arena;
 import net.slipcor.pvparena.arena.ArenaPlayer;
+import net.slipcor.pvparena.classes.PACheck;
 import net.slipcor.pvparena.commands.CommandTree;
 import net.slipcor.pvparena.core.Language;
 import net.slipcor.pvparena.core.Language.MSG;
@@ -24,10 +25,11 @@ public class DuelManager extends ArenaModule {
 
     @Override
     public String version() {
-        return "v1.3.1.25";
+        return "v1.3.1.39";
     }
 
-    private String duel = null;
+    private String duelSender = null;
+    private String duelReceiver = null;
 
     @Override
     public boolean checkCommand(final String s) {
@@ -57,6 +59,16 @@ public class DuelManager extends ArenaModule {
     }
 
     @Override
+    public PACheck checkJoin(CommandSender sender, PACheck res, boolean spectate) {
+        if (!spectate) {
+            if (!sender.getName().equals(this.duelReceiver) && !sender.getName().equals(this.duelSender)) {
+                res.setError(this, Language.parse(arena, MSG.MODULE_DUEL_NODIRECTJOIN));
+            }
+        }
+        return null;
+    }
+
+    @Override
     public void commitCommand(final CommandSender sender, final String[] args) {
         if (arena.isFightInProgress()) {
             arena.msg(sender,
@@ -79,7 +91,7 @@ public class DuelManager extends ArenaModule {
             arenaName = arena.getName();
         }
         if ("duel".equals(args[0].toLowerCase()) && args.length > 1) {
-            if (sender.getName().equals(duel)) {
+            if (sender.getName().equals(duelSender)) {
                 arena.msg(sender, Language.parse(MSG.MODULE_DUEL_REQUESTED_ALREADY));
             } else {
                 final Player p = Bukkit.getPlayer(args[1]);
@@ -90,42 +102,46 @@ public class DuelManager extends ArenaModule {
                 arena.msg(p, Language.parse(MSG.MODULE_DUEL_ANNOUNCE, sender.getName(), arenaName));
                 arena.msg(p, Language.parse(MSG.MODULE_DUEL_ANNOUNCE2, sender.getName(), arenaName));
                 arena.msg(sender, Language.parse(MSG.MODULE_DUEL_REQUESTED, p.getName()));
-                duel = sender.getName();
+                duelSender = sender.getName();
+                duelReceiver = p.getName();
                 class LaterRunner implements Runnable {
                     @Override
                     public void run() {
-                        if (duel != null) {
+                        if (duelSender != null) {
                             if (p != null) {
                                 arena.msg(p, Language.parse(MSG.MODULE_DUEL_REQUEST_EXPIRED_RECEIVER));
                             }
                             if (sender != null) {
                                 arena.msg(sender, Language.parse(MSG.MODULE_DUEL_REQUEST_EXPIRED_SENDER));
                             }
-                            duel = null;
+                            duelSender = null;
+                            duelReceiver = null;
                         }
                     }
                 }
                 Bukkit.getScheduler().scheduleSyncDelayedTask(PVPArena.instance, new LaterRunner(), 1200L);
             }
         } else if ("accept".equals(args[0].toLowerCase())) {
-            if (duel != null && !arena.isFightInProgress()) {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(PVPArena.instance, new DuelRunnable(this, duel, sender.getName()), 500L);
-                final Player p = Bukkit.getPlayer(duel);
+            if (duelSender != null && !arena.isFightInProgress()) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(PVPArena.instance, new DuelRunnable(this, duelSender, sender.getName()), 500L);
+                final Player p = Bukkit.getPlayer(duelSender);
                 if (p != null) {
                     arena.msg(p, Language.parse(MSG.MODULE_DUEL_ACCEPTED, sender.getName()));
                 }
-                duel = null;
+                duelSender = null;
+                duelReceiver = null;
             } else if (arena.isFightInProgress()) {
                 arena.msg(sender,
                         Language.parse(MSG.ERROR_FIGHT_IN_PROGRESS));
             }
-        } else if ("decline".equals(args[0].toLowerCase()) && duel != null) {
-            ArenaPlayer p = ArenaPlayer.parsePlayer(duel);
+        } else if ("decline".equals(args[0].toLowerCase()) && duelSender != null) {
+            ArenaPlayer p = ArenaPlayer.parsePlayer(duelSender);
             if (p != null && p.get() != null){
                 arena.msg(p.get(), Language.parse(MSG.MODULE_DUEL_DECLINED_SENDER));
             }
             arena.msg(sender, Language.parse(MSG.MODULE_DUEL_DECLINED_RECEIVER));
-            duel = null;
+            duelSender = null;
+            duelReceiver = null;
         }
     }
 
